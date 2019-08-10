496ac7cfbb7930e6a54918725ad6755f52b060a2
Jannis R <mail@jannisr.de>
2019-08-10 19:29:16 +0200

    quiet

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@ set -e
 set -o pipefail
 set -x
 
+git clone \
+	-b install-script --single-branch --depth 1 \
+	https://github.com/derhuerst/quiet.git quiet
+cd quiet
+./install-dependencies.sh
+
 # todo
+./bootstrap
+# ./bootstrap_js
